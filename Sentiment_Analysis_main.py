7bc4c4f8ada8205563dcf41a7a19968a1bbc73b3
阳宇翔 <18310206637@163.com>
2021-04-14 22:47:30 +0800

    更改

--- Sentiment_Analysis_main.py
+++ Sentiment_Analysis_main.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader,Dataset
-import tqdm
+from tqdm import tqdm
 from Sentiment_Analysis_DataProcess import prepare_data,build_word2vec,Data_set
 from sklearn.metrics import confusion_matrix,f1_score,recall_score
 import os
@@ -15,7 +15,7 @@ from Sentiment_Analysis_eval import val_accuary
 import logging
 import numpy as np
 from lr_schedular import StepLR
-
+from pytorchtools import EarlyStopping
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s %(levelname)s %(message)s',
                     datefmt='%a %d %b %Y %H:%M:%S'
@@ -28,17 +28,30 @@ def train(train_dataloader, model, device, epoches, lr):
     model.train()
     model = model.to(device)
     print(model)
-    optimizer = optim.Adagrad(model.parameters(), lr=lr, initial_accumulator_value=0.1)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+#     optimizer = optim.Adagrad(model.parameters(), lr=lr, initial_accumulator_value=0.1)
     criterion = nn.CrossEntropyLoss()
-    scheduler = StepLR(optimizer, step_size=10, gamma=0.2)  # 学习率调整
-    best_acc = -np.inf
-    for epoch in range(epoches):  # 一个epoch可以认为是一次训练循环
-        train_loss = 0.0
+#     scheduler = StepLR(optimizer, step_size=10, gamma=0.2)  # 学习率调整
+    
+    if os.path.exists(os.path.join(Config.model_state_dict_path, "best_val_result.txt")):
+        with open(os.path.join(Config.model_state_dict_path, "best_val_result.txt")) as f:
+            best_acc = float(list(f.readlines())[0].split("=")[-1])
+            logging.info("the best val_acc of last train is: %s " % best_acc)
+    else:
+        best_acc = -np.inf
+        
+    early_stopping = EarlyStopping(patience=Config.patience, verbose=True)
+    
+    train_avg_losses = []
+    val_avg_losses = []
+    for epoch in tqdm(range(epoches)):  # 一个epoch可以认为是一次训练循环
+        batch_losses = []
         correct = 0
         total = 0
+        logging.info("epoch: %s, batches: %s" % (epoch, len(train_dataloader)))
 
-        train_dataloader = tqdm.tqdm(train_dataloader)
-        train_dataloader.set_description('[%s%04d/%04d %s%f]' % ('Epoch:', epoch + 1, epoches, 'lr:', scheduler.get_last_lr()[0]))
+#         train_dataloader = tqdm(train_dataloader)
+#         train_dataloader.set_description('[%s%04d/%04d %s%f]' % ('Epoch:', epoch + 1, epoches, 'lr:', scheduler.get_last_lr()[0]))
         for i, data_ in (enumerate(train_dataloader)):
             optimizer.zero_grad()
             input_, target = data_[0], data_[1]
@@ -52,29 +65,50 @@ def train(train_dataloader, model, device, epoches, lr):
             loss = criterion(output, target)
             loss.backward()
             optimizer.step()
-            train_loss += loss.item()
+            
+            batch_losses.append(loss.item())
+            
             _, predicted = torch.max(output, 1)
             #print(predicted.shape)
             # 此处的size()类似numpy的shape: np.shape(train_images)[0]
             total += target.size(0)
             #print(target.shape)
             correct += (predicted == target).sum().item()
-            F1 = f1_score(target.cpu(), predicted.cpu(),
-                          average='weighted')
-            Recall = recall_score(
-                target.cpu(), predicted.cpu(), average='micro')
+            F1 = f1_score(target.cpu(), predicted.cpu())
+            Recall = recall_score(target.cpu(), predicted.cpu())
             #CM=confusion_matrix(target.cpu(),predicted.cpu())
-            val_loss, val_acc, val_f1, val_recall = val_accuary(
-                model, val_dataloader, device, criterion)
-            postfix = {'train_loss: {:.5f}, train_acc: {:.3f}%, train_F1: {:.3f}%, train_Recall: {:.3f}%, val_loss: {:.5f}, val_acc: {:.3f}%, val_F1：{:.3f}%, val_Recall {:.3f}%'.format(
-                train_loss / (i + 1), 100 * correct / total, 100*F1, 100 * Recall, val_loss, val_acc, val_f1, val_recall)}
-            train_dataloader.set_postfix(log=postfix)
+            
+            
+            if (i%10==0):
+                postfix = {'epoch: {}, batch: {}, train_loss: {:.5f}, acc: {:.3f}%, F1: {:.3f}%, Recall: {:.3f}%'.format(epoch, i, \
+                                                                np.mean(batch_losses), 100 * correct / total, 100*F1, 100 * Recall)}
+                logging.info(postfix)
 
-            if val_acc > best_acc:
-                best_acc = val_acc
-                if os.path.exists(Config.model_state_dict_path) == False:
-                    os.mkdir(Config.model_state_dict_path)
-                torch.save(model, os.path.join(Config.model_state_dict_path, 'sen_model_best_val_acc_{:.3f}%.pkl'.format(val_acc)))
+        mean_val_loss, val_acc, val_f1, val_recall = val_accuary(model, val_dataloader, device, criterion)
+        logging.info('epoch: {}, val_loss: {:.5f}, acc: {:.3f}%, F1：{:.3f}%, Recall: {:.3f}%'.format(epoch, mean_val_loss,\
+                                                                                             100*val_acc,100*val_f1,100*val_recall))
+        if val_acc > best_acc:
+            best_acc = val_acc
+            if os.path.exists(Config.model_state_dict_path) == False: 
+                os.mkdir(Config.model_state_dict_path)
+            torch.save(model, os.path.join(Config.model_state_dict_path, 'sen_model_best.pkl'))
+            with open(os.path.join(Config.model_state_dict_path, "best_val_result.txt"), "w") as f:
+                f.write("best val_acc=%s\n"%val_acc)
+                f.write("best val_f1=%s\n"%val_f1)
+                f.write("best val_recall=%s\n"%val_recall)
+                    
+        train_avg_losses.append(np.mean(train_avg_losses))
+        val_avg_losses.append(mean_val_loss)
+        # early_stopping needs the validation loss to check if it has decresed, 
+        # and if it has, it will make a checkpoint of the current model
+        early_stopping(mean_val_loss, model)
+        
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+            
+    model.load_state_dict(torch.load('checkpoint.pt'))
+    return  model, train_avg_losses, val_avg_losses
 
 
 if __name__ == '__main__':
@@ -134,11 +168,10 @@ if __name__ == '__main__':
                           Config.hidden_dim, Config.num_layers, Config.drop_keep_prob, Config.n_class, Config.bidirectional)
 
     #训练
-    train(train_dataloader, model=model, device=device,
-          epoches=Config.n_epoch, lr=Config.lr)
+    model, train_avg_losses, val_avg_losses = train(train_dataloader, model=model, device=device, epoches=Config.n_epoch, lr=Config.lr)
 
 
     #保存模型
     if os.path.exists(Config.model_state_dict_path) == False:
         os.mkdir(Config.model_state_dict_path)
-    torch.save(model, Config.model_state_dict_path)
+    torch.save(model, os.path.join(Config.model_state_dict_path, "sen_model.pkl"))
